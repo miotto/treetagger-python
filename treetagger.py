409151d495db38291074272e7c630763271c2a90
rrafaelpinto <rrafaelpinto@gmail.com>
2015-03-16 14:01:29 -0300

    Update treetagger.py

--- treetagger.py
+++ treetagger.py
@@ -23,8 +23,8 @@ def tUoB(obj, encoding='utf-8'):
 _treetagger_url = 'http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/'
 
 _treetagger_languages = {
-u'latin-1':['bulgarian', 'dutch', 'english', 'estonian', 'french', 'german', 'greek', 'italian', 'latin', 'russian', 'spanish', 'swahili'],
-u'utf8' : ['french', 'german', 'greek', 'italian', 'spanish', 'portuguese']}
+u'latin-1':['bulgarian', 'dutch', 'estonian', 'french', 'german', 'greek', 'italian', 'latin', 'russian', 'spanish', 'swahili'],
+u'utf8' : ['french', 'german', 'greek', 'italian', 'spanish', 'english', 'portuguese']}
 
 """The default encoding used by TreeTagger: utf8. u'' means latin-1; ISO-8859-1"""
 _treetagger_charset = [u'utf8', u'latin-1']
